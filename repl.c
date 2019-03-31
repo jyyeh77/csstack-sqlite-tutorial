eb09f75496a1e706dc2158228107b4859dff713b
Justin Yeh <jyyeh77@gmail.com>
2019-03-31 19:55:39 -0400

    (Justin) restart repl

--- repl.c
+++ repl.c
@@ -1,76 +1,62 @@
-#include <stddef.h>
 #include <sys/types.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
-// sqlite REPL
 struct InputBuffer_t {
-  char* buffer;
-  size_t buffer_length; // unsigned type
-  ssize_t input_length; // signed type
+    // buffer is a pointer to data type char
+    char* buffer;
+    // size_t is unsigned type, can't repr negative values, used for lengths
+    size_t buffer_length;
+    // ssize_t is signed type, can represent -1
+    ssize_t input_length;
 };
+// alias type InputBuffer_t as InputBuffer
 typedef struct InputBuffer_t InputBuffer;
 
-enum MetaCommandResult_t {
-  META_COMMAND_SUCCESS,
-  META_COMMAND_UNRECOGNIZED_COMMAND
-};
-typedef enum MetaCommandResult_t 
-
 InputBuffer* new_input_buffer() {
-  // allocate memory for new input buffer
-  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
-  input_buffer->buffer = NULL;
-  input_buffer->buffer_length = 0;
-  input_buffer->input_length = 0;
+    // malloc returns pointer to allocate memory for input buffer
+    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL; // from stdlib
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
 
-  return input_buffer;
+    return input_buffer;
 }
 
 void print_prompt() {
-  printf("db > ");
+    printf("db > ");
 }
 
-// **lineptr is a pointer to a pointer to value of type char
+// lineptr -> pointer to variable that points to buffer w/read line
+// equivalent to char *lineptr[]
+// n -> pointer to variable that saves size of allocated buffer
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 void read_input(InputBuffer* input_buffer) {
-  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-  if (bytes_read <= 0) {
-    printf("error reading input\n");
-    exit(EXIT_FAILURE);
-  }
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
 
-  // ignore trailing new line
-  input_buffer->input_length = bytes_read - 1;
-  input_buffer->buffer[bytes_read - 1] = 0;
+    // ignore trailing newline
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
 }
 
 int main(int argc, char* argv[]) {
-  InputBuffer* input_buffer = new_input_buffer();
-  while (true) {
-    print_prompt();
-    read_input(input_buffer);
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
 
-    // get buffer pointer from struct that input_buffer points to
-    // if input buffer is '.exit', exit success
-    /*if (strcmp(input_buffer->buffer, ".exit") == 0) {*/
-      /*exit(EXIT_SUCCESS);*/
-    /*} else {*/
-      /*printf("Unrecognized command '%s'.\n", input_buffer->buffer);*/
-    /*}*/
-
-    if (input_buffer->buffer[0] == '.') {
-      switch (do_meta_command(input_buffer)) {
-        case (META_COMMAND_SUCCESS):
-          continue;
-        case (META_COMMAND_UNRECOGNIZED_COMMAND):
-          printf("Unrecognized command '%s'\n", input_buffer->buffer[0])
-          continue;
-      }
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
     }
-  }
 }
